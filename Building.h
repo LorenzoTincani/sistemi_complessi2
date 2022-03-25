679286479e7bc65d9c06d7a043e6be052d8aefc5
nettilaria <ilaria.netti@studio.unibo.it>
2022-03-25 22:53:45 +0100

    Modifca

--- Building.h
+++ Building.h
@@ -11,16 +11,21 @@ class Building
 {
 private:
     BuildingType type_ = BuildingType::H;
-    double need_ = 0.0;
+    double need_ = 0.0; 
     double entry_potential_ = 0.0; //settato a zero, perchè definito dalla dinamica
+    bool sorting_link_ = false;
+
 public:
     Building(BuildingType type, double need, double entry_potential);
     Building() = default; //costruttore di default insieme alle condizioni uguali nel private
+
     BuildingType GetType() const;
     double GetNeed() const;
     double GetEntryPotential() const;
+    bool GetSortingLink() const;
     void SetEntryPotential(double entry_potential);
     void SetNeed(double need);
     void SetType(BuildingType type);
+    void SetSortingLink(bool Sorting);
 };
 #endif
