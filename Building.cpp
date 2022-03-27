75abf58ae41bb412e18bc0f6fc4d4d8befb43d6d
lorenzo tincani <lorenzotincani@outlook.it>
2022-03-27 22:46:42 +0200

    da mettere a posto case non collegate

--- Building.cpp
+++ Building.cpp
@@ -1,4 +1,5 @@
 #include "Building.h"
+#include "algorithm"
 Building::Building(BuildingType type, double need, double entry_potential) : type_{type}, need_{need}, entry_potential_{entry_potential}
 {
 }
@@ -35,9 +36,32 @@ void Building::SetSortingLink(bool Sorting)
 {
     sorting_link_ = Sorting;
 }
-double Building::OutputPotential()const
+double Building::OutputPotential() const
 {
     return (entry_potential_ - need_);
 }
+void Bulding::SetPath(int path, int distance)
+{
+    path_[path] = distance;
+}
+std::vector<int> Building::GetPath() const
+{
 
-// sus.eseguibile
+    return path_;
+}
+
+int GetPathNunmber () const{
+
+   return path_.size();
+
+}
+int Bulding::GetMinPath()const{
+    return path.min();
+};
+int Building::GetMaxPath()const{
+    return path.max();
+};
+int GetPathLength(int path)const{
+    return path_[path];
+s
+}
