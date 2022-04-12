76c3dc0833febb328d0ff51e9891043f3c0d8583
lorenzo tincani <lorenzotincani@outlook.it>
2022-04-12 22:31:30 +0200

    vedi coomento in fondo

--- main.cpp
+++ main.cpp
@@ -634,8 +634,12 @@ int main()
             condition_house_max++;
 
                 bool full = false;
-                for (int y = 0; localLinkH <= 0.06 && full == false;)
+                for (int y = 0; full == false;)
                 { // in un for cerco nodo e nell'altro collego
+
+                    if( localLinkH <= 0.06){
+                        break;
+                    }
 
                     for (int m = 0; m < nofHouse;)
                     {
