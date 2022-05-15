af119b18211849205a9f501e8fb9733d5f0382e1
nettilaria <ilaria.netti@studio.unibo.it>
2022-05-15 23:10:21 +0200

    Cambiamenti

--- main.cpp
+++ main.cpp
@@ -17,230 +17,271 @@ Nella matrice di adiacenza:
 - 3 = smistamento - centrale
 - 4 = casa -smistamento
  **************************************************************  */
-// namespace matrix {
-
 int main()
 {
-    int N =10;
+    int N = 10;
     Matrix adjiacency_matrix{N};
-  //  std::vector<std::vector<int>> ADJmatrix(N,std::vector<int>(N,0));
     adjiacency_matrix.create();
-    
+    adjiacency_matrix.control_for_matrix();
+
     std::cout << "nofcentral: " << adjiacency_matrix.getNofCentral() << std::endl;
     std::cout << "nofHouse: " << adjiacency_matrix.getNofHouse() << std::endl;
     std::cout << "nofSorting: " << adjiacency_matrix.getNofSorting() << std::endl;
-    /*for(int i=0;i<100;i++){
-        if( adjiacency_matrix(i).GetType()==BuildingType::S){
-         adjiacency_matrix(i).PrintPath(0,false);
+
+
+    for(int i=0;i<N;i++){
+        if(adjiacency_matrix(i).GetType() == BuildingType::H){
+
+        for(int j=0;j<N;j++){
+            if(adjiacency_matrix(j).GetType() == BuildingType::C){
+                 adjiacency_matrix.printAllPaths(i, j);
+
+            }
+
         }
-
-    }*/
-
-   
-   
+        }
+    }
+    std::cout<<"..........................................\n";
 
   
-
-    std::cout << "*********************CONTROLLI COLLEGAMENTI SMISTAMENTI-CASE************************************" << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "---------------Collegamenti casa-casa prima dei controlli" << std::endl;
-    std::cout << std::endl;
-
-   /*for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            //std::cout<<"Inizio problema";
-          //  ADJmatrix[i].push_back(0);
-          std::cout<<ADJmatrix[i][j]<<" ";
-        }
-        std::cout<<std::endl;
-    }*/
-
-
-  /* for (int k = 0; k < N; k++)
-    {
-        if (nodes[k].GetType() == BuildingType::H)
-        {
-            std::cout << "Case collegate alla " << k << "esima casa :" << nodes[k].GetNofHouseLink();
-            // nodes[k].Print(0, 'H', true);
-        }
-        std::cout << std::endl;
-    }*/
-
-   // std::cout << std::endl;
-
-   /* for (int k = 0; k < N; ++k)
-    {
-        if (nodes[k].GetType() == BuildingType::S)
-        {
-            double localLinkH = static_cast<double>(nodes[k].GetNofHouseLink()) / static_cast<double>(nofHouse);
-            std::cout << "Tasso di collegamento sistamento " << k << "-esimo dopo : " << localLinkH << std::endl;
-        }
-    }
-    std::cout << std::endl;
-
-    std::cout << "---------------Collegamenti casa-casa dopo i controlli" << std::endl;
-    std::cout << std::endl;
-
-    for (int k = 0; k < N; k++)
-    {
-        if (nodes[k].GetType() == BuildingType::H)
-        {
-            std::cout << "Case collegate alla casa " << k << "esima: " << nodes[k].GetNofHouseLink();
-            // nodes[k].Print(0, 'H', true);
-        }
-        std::cout << std::endl;
-    }
-    std::cout << "----------------------" << std::endl;
-    std::cout << std::endl;
-    std::cout << "*********************************************************" << std::endl;*/
-
-  /*for (int i = 0; i < N; i++)
-    {
-        for (int k = 0; k < N; k++)
-        {
-            if (adjiacency_matrix(i,k).GetType() == LinkType::N) // null
-            {
-                //printf("\033[33m0 ");
-               // ADJmatrix[i].push_back(0);
-              
-
-            }
-            else if (adjiacency_matrix(i,k).GetType() == LinkType::SH) // hh
-            {
-                // std::cout << "       ";
-              //  printf("\033[31m1 ");
-                // ADJmatrix[i].push_back(1);
-                ADJmatrix[i][k]=1;
-            }
-            else if (adjiacency_matrix(i,k).GetType() == LinkType::M) // ss
-            {
-                // std::cout << "       ";
-              //  printf("\033[32m2 ");
-              //   ADJmatrix[i].push_back(1);
-              ADJmatrix[i][k]=1;
-            }
-            else if (adjiacency_matrix(i,k).GetType() == LinkType::B) // cs
-            {
-                // std::cout << "       ";
-              //  printf("\033[36m3 ");
-                ADJmatrix[i][k]=1;
-            }
-            else if (adjiacency_matrix(i,k).GetType() == LinkType::SS)
-            { // hs
-             //   printf("\033[37m4 ");
-                ADJmatrix[i][k]=1;
-            }
-            else
-            { // E' giusto vedere se viene generato qualche numero che non sia tra quelli contemplati.
-             //   printf("\033[35m7 ");
-            }
-            //printf("\033[0m");
-        }
-
-        std::cout << std::endl;
-    }*/
- /*   int nofHouselinkedS = 0;
-    int nofSNOlinked = 0;
-
-    for (int k = 0; k < N; k++)
-    {
-        auto nodeType = nodes[k].GetType();
-        char type = ' ';
-        if (nodeType == BuildingType::S)
-        {
-            type = 'S';
-            if (nodes[k].GetNofHouseLink() == 0)
-            {
-                nofSNOlinked++;
-            }
-        }
-        else if (nodeType == BuildingType::H)
-        {
-            type = 'H';
-            nofHouselinkedS += nodes[k].GetNofSortingLink();
-        }
-        else
-        {
-            type = 'C';
-        }
-        std::cout << "Type of node " << k << " " << type << std::endl;
-        std::cout << "Sorting Link of " << nodes[k].GetNofSortingLink() << std::endl;
-        std::cout << "House Link of " << nodes[k].GetNofHouseLink() << std::endl;
-        std::cout << "Central Link of " << nodes[k].GetNofCentralLink() << std::endl;
-        std::cout << "------------------------------" << std::endl;
-    }
-    std::cout << "nofSmalllink :" << nofSmalllink << "\n";
-    std::cout << "nofHouseSortingLink: " << nofHSLink << "\n";
-    std::cout << "nofMediumlink :" << nofMediumlink << "\n";
-    std::cout << "nofBiglink :" << nofBiglink << "\n";
-    std::cout << std::endl;
-    std::cout << "****************CONTROLLI***************" << std::endl;
-    std::cout << "Su " << nofHouse << " case ce ne sono " << nofHouselinkedS << " collegate ad uno smistamento"
-              << "\n";
-    std::cout << "Su " << nofSorting << " smistamenti ce ne sono " << nofSNOlinked << "  collegati a zero case"
-              << "\n";
-    std::cout << "****************************************" << std::endl;
-    std::cout << "H con tasso di collegamento basso: " << condition_house_min << " su " << nofHouse << std::endl;
-    std::cout << "H con tasso di collegamento elevato: " << condition_house_max << " su " << nofHouse << std::endl;
-    std::cout << "H con tasso di collegamento corretto: " << condition_house_ok << " su " << nofHouse << std::endl;
-
-    // std::cout << "nofCentralLink :" << linkCentral << "\n";
-
-    // ********************SCRITTURA MATRICE SU FILE******************
-    std::ofstream adjmatrix;
-    adjmatrix.open("adjmatrix.txt"); // Viene creato un file di nome "adjmatrix.txt"
     for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < N; j++)
+        if (adjiacency_matrix(i).GetType() == BuildingType::H)
         {
-            adjmatrix << adj_matrix[i][j].GetNumber(); // Vengono scritti i numeri indentati, in modo da avere la visione della matrice
-            adjmatrix << " ";
+            // std::cout<<"entrato nell'if casa del main\n";
+            for (int j = 0; j < N; j++)
+            {
+                if (adjiacency_matrix(j).GetType() == BuildingType::C)
+                {
+                    if (adjiacency_matrix.isConnected(i, j) == true)
+
+                    {
+                        adjiacency_matrix.printAllPaths(i, j);
+                    }
+                    else
+                    {
+                        std::cout << i << " No collegamenti" << '\n';
+                    }
+                }
+            }
+
+            /* if ((adjiacency_matrix(i).GetNofSortingLink() == 0) && (adjiacency_matrix(i).GetNofHouseLink()==0))
+             {
+                 std::cout<<"(PUNTO ISOLATO) "<<i<<"\n";
+                   for (int j = 0; j < N; j++)
+             {
+
+                 if (adjiacency_matrix(j).GetType() == BuildingType::C)
+                 {
+                     adjiacency_matrix.printAllPaths(i, j);
+                 }
+
+             }
+
+             }
+             else if (adjiacency_matrix(i).GetNofSortingLink() == 0){
+                 std::cout<<"(CASA NO COLLEGATA A Smistamento) "<<i<<"\n";
+
+             }
+             else if(adjiacency_matrix(i).GetNofHouseLink()==0)
+
+
+             {
+                 std::cout<<"(CASA NO COLLEGATA A CASA) "<<i<<"\n";
+             }*/
+            /*else{
+                std::cout<<"casa "<<i<<std::endl;
+
+
+
+            for (int j = 0; j < N; j++)
+            {
+
+                if (adjiacency_matrix(j).GetType() == BuildingType::C)
+                {
+                    adjiacency_matrix.printAllPaths(i, j);
+                }
+
+
+            }
+            }
+
+        }*/
         }
-        adjmatrix << std::endl;
-    }
-    adjmatrix.close();
+        /* else if (adjiacency_matrix(i).GetType() == BuildingType::S)
+         {
+             std::cout << "Smistamento: "<<i<<'\n';
 
-*/
+         }
+         else if (adjiacency_matrix(i).GetType() == BuildingType::H)
+         {
+             std::cout << "Casa: "<<i<<"\n";
+         }*/
+      /*  else
+        {
+            std::cout << "non sta stampando :)\n";
+        }*/
+   }
+
+    /*for(int i=0;i<N;i++){
+         for(int j=0;j<N;j++){
+             //std::cout<<"Inizio problema";
+           //  ADJmatrix[i].push_back(0);
+           std::cout<<ADJmatrix[i][j]<<" ";
+         }
+         std::cout<<std::endl;
+     }*/
+
+    /* for (int k = 0; k < N; k++)
+      {
+          if (nodes[k].GetType() == BuildingType::H)
+          {
+              std::cout << "Case collegate alla " << k << "esima casa :" << nodes[k].GetNofHouseLink();
+              // nodes[k].Print(0, 'H', true);
+          }
+          std::cout << std::endl;
+      }*/
+
+    // std::cout << std::endl;
+
+    /* for (int k = 0; k < N; ++k)
+     {
+         if (nodes[k].GetType() == BuildingType::S)
+         {
+             double localLinkH = static_cast<double>(nodes[k].GetNofHouseLink()) / static_cast<double>(nofHouse);
+             std::cout << "Tasso di collegamento sistamento " << k << "-esimo dopo : " << localLinkH << std::endl;
+         }
+     }
+     std::cout << std::endl;
+
+     std::cout << "---------------Collegamenti casa-casa dopo i controlli" << std::endl;
+     std::cout << std::endl;
+
+     for (int k = 0; k < N; k++)
+     {
+         if (nodes[k].GetType() == BuildingType::H)
+         {
+             std::cout << "Case collegate alla casa " << k << "esima: " << nodes[k].GetNofHouseLink();
+             // nodes[k].Print(0, 'H', true);
+         }
+         std::cout << std::endl;
+     }
+     std::cout << "----------------------" << std::endl;
+     std::cout << std::endl;
+     std::cout << "*********************************************************" << std::endl;*/
+
+    /*for (int i = 0; i < N; i++)
+      {
+          for (int k = 0; k < N; k++)
+          {
+              if (adjiacency_matrix(i,k).GetType() == LinkType::N) // null
+              {
+                  //printf("\033[33m0 ");
+                 // ADJmatrix[i].push_back(0);
 
 
+              }
+              else if (adjiacency_matrix(i,k).GetType() == LinkType::SH) // hh
+              {
+                  // std::cout << "       ";
+                //  printf("\033[31m1 ");
+                  // ADJmatrix[i].push_back(1);
+                  ADJmatrix[i][k]=1;
+              }
+              else if (adjiacency_matrix(i,k).GetType() == LinkType::M) // ss
+              {
+                  // std::cout << "       ";
+                //  printf("\033[32m2 ");
+                //   ADJmatrix[i].push_back(1);
+                ADJmatrix[i][k]=1;
+              }
+              else if (adjiacency_matrix(i,k).GetType() == LinkType::B) // cs
+              {
+                  // std::cout << "       ";
+                //  printf("\033[36m3 ");
+                  ADJmatrix[i][k]=1;
+              }
+              else if (adjiacency_matrix(i,k).GetType() == LinkType::SS)
+              { // hs
+               //   printf("\033[37m4 ");
+                  ADJmatrix[i][k]=1;
+              }
+              else
+              { // E' giusto vedere se viene generato qualche numero che non sia tra quelli contemplati.
+               //   printf("\033[35m7 ");
+              }
+              //printf("\033[0m");
+          }
 
+          std::cout << std::endl;
+      }*/
+    /*   int nofHouselinkedS = 0;
+       int nofSNOlinked = 0;
 
+       for (int k = 0; k < N; k++)
+       {
+           auto nodeType = nodes[k].GetType();
+           char type = ' ';
+           if (nodeType == BuildingType::S)
+           {
+               type = 'S';
+               if (nodes[k].GetNofHouseLink() == 0)
+               {
+                   nofSNOlinked++;
+               }
+           }
+           else if (nodeType == BuildingType::H)
+           {
+               type = 'H';
+               nofHouselinkedS += nodes[k].GetNofSortingLink();
+           }
+           else
+           {
+               type = 'C';
+           }
+           std::cout << "Type of node " << k << " " << type << std::endl;
+           std::cout << "Sorting Link of " << nodes[k].GetNofSortingLink() << std::endl;
+           std::cout << "House Link of " << nodes[k].GetNofHouseLink() << std::endl;
+           std::cout << "Central Link of " << nodes[k].GetNofCentralLink() << std::endl;
+           std::cout << "------------------------------" << std::endl;
+       }
+       std::cout << "nofSmalllink :" << nofSmalllink << "\n";
+       std::cout << "nofHouseSortingLink: " << nofHSLink << "\n";
+       std::cout << "nofMediumlink :" << nofMediumlink << "\n";
+       std::cout << "nofBiglink :" << nofBiglink << "\n";
+       std::cout << std::endl;
+       std::cout << "****************CONTROLLI***************" << std::endl;
+       std::cout << "Su " << nofHouse << " case ce ne sono " << nofHouselinkedS << " collegate ad uno smistamento"
+                 << "\n";
+       std::cout << "Su " << nofSorting << " smistamenti ce ne sono " << nofSNOlinked << "  collegati a zero case"
+                 << "\n";
+       std::cout << "****************************************" << std::endl;
+       std::cout << "H con tasso di collegamento basso: " << condition_house_min << " su " << nofHouse << std::endl;
+       std::cout << "H con tasso di collegamento elevato: " << condition_house_max << " su " << nofHouse << std::endl;
+       std::cout << "H con tasso di collegamento corretto: " << condition_house_ok << " su " << nofHouse << std::endl;
 
+       // std::cout << "nofCentralLink :" << linkCentral << "\n";
 
+       // ********************SCRITTURA MATRICE SU FILE******************
+       std::ofstream adjmatrix;
+       adjmatrix.open("adjmatrix.txt"); // Viene creato un file di nome "adjmatrix.txt"
+       for (int i = 0; i < N; i++)
+       {
+           for (int j = 0; j < N; j++)
+           {
+               adjmatrix << adj_matrix[i][j].GetNumber(); // Vengono scritti i numeri indentati, in modo da avere la visione della matrice
+               adjmatrix << " ";
+           }
+           adjmatrix << std::endl;
+       }
+       adjmatrix.close();
 
-
-
-
-
-
-
-	/*
-	We will find path matrix for this graph
-
-		     5
-		     ^
-      		|
-		3 -> 4
-		^    ^
-		|    |
-		1 -> 2
-
-	*/
-
-	// Example Adjacency matrix, row 0 and column 0 doesn't contribute to path matrix
-
-	
-
-
-
-
-
-
-
-
-
+   */
 }
 
+// problema che vede i sorting come case quando si stampa il number_of, ci sono dei ghetti e dei punti isolati, risolvere
+// andando a controllare gli argomenti della list relativi a quel nodo e guardare se nessuno arriva a una centrale, se è cosi
+//  creare un path diretto che lo colleghi con uno smistamento a caso
+// Link casa-smistamento
 
-//FARE ADD EDGE ALL'INTERNO DI CREATE
+// update 13/05 riguardare i metodi fatti
