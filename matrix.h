8189b4950e23abd20703abc7f01f91d8096104ab
nettilaria <83864422+nettilaria@users.noreply.github.com>
2022-09-07 23:44:04 +0200

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -12,30 +12,24 @@
 #include <algorithm>
 #include <fstream>
 #include <map>
-
-/*struct record
-{
-  std::string Pup;
-  std::string Dropoff;
-  //Qui bisogna usare un vettore in quanto si hanno più di due variabili. 
-  
-};*/
-
 class Matrix
 {
 private:
   int N = 0;
   std::vector<std::vector<Link>> adj_matrix{};
   std::list<int> *adj = new std::list<int>[N]; // native array del c++ composto da list
-  std::map<int,std::vector<int>> records{};
+  std::map<int, std::vector<int>> records{};
   void printAllPathsUtil(int, int, bool[], int[], int &, std::ostream &);
+    void printAllPathsUtil(int, int, bool[], int[], int &, std::vector<std::vector<int>>& local);
   bool isConnectedUtil(int, int, bool[], int[], int &, int &);
+  
+  std::vector<int> linked_building{};
   std::vector<Building> nodes{};
   std::vector<int> Centrall{}; // vettore che tiene gli indici delle centrali di nodes
   std::vector<int> Sorting{};  // vettore che tiene gli indici degli smistamenti di nodes
   std::vector<int> House{};    // vettore che tiene gli indici delle case di nodes
+  void findIsolated(int starting_building, std::vector<int> building_to_scan, std::vector<int> &local_linked_building);
   double Total_potential = 0.0;
-  
 
 public:
   Matrix(int n);
@@ -45,15 +39,23 @@ public:
   // Nel programma a cui ci stiamo ispirando, il metdodo viene utilizzato sulla classe nel main.
 
   void printAllPaths(int s, int d, std::ostream &);
+    void printAllPaths(int s, int d, std::vector<std::vector<int>>& local);
   bool isConnected(int s, int d);
+
   void create();
   void control_for_matrix();
+  void control_for_matrix2();
   void control_path();
   void transient();
+  void evolve();
   int getNofHouse() const;
   int getNofSorting() const;
   int getNofCentral() const;
   void fillRecords();
+  void PrintNodes() const;
+  void PrintLinkedBuilding() const;
+  void PrintADJmatrix()const;
+  void PrintADJLoad()const;
 
   Building &operator()(int i);
   Link &operator()(int i, int j);
